294c9d3fb3920ca5a110b69d55bf23a9476cb1c5
Makkai98 <makkaimatyas00@gmail.com>
2019-11-19 12:22:55 +0200

    Group-, Question- and AnswerListFragment

--- app/src/main/java/com/example/planningpokerapp/Fragments/QuestionFragment.java
+++ app/src/main/java/com/example/planningpokerapp/Fragments/QuestionFragment.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.planningpokerapp.R;
 
 public class QuestionFragment extends Fragment {
+
 
     public QuestionFragment(){};
 
@@ -26,10 +28,22 @@ public class QuestionFragment extends Fragment {
         View view = inflater.inflate(
                 R.layout.fragment_question, container, false);
 
+        Button submit_button = view.findViewById(R.id.button_submitanswers);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GroupListFragment Gfragment = new GroupListFragment();
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, Gfragment);
+                fr.addToBackStack(null);
+                fr.commit();
+            }
+        });
 
 
         return  view;
     }
+
 
 
 }
---
+++ app/src/main/java/com/example/planningpokerapp/Fragments/QuestionListFragment.java
@@ -0,0 +1,32 @@
+package com.example.planningpokerapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.planningpokerapp.R;
+
+public class QuestionListFragment extends Fragment {
+
+    public QuestionListFragment(){};
+
+    public void Oncreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(
+                R.layout.fragment_questionlist, container, false);
+
+
+
+        return  view;
+    }
+}
